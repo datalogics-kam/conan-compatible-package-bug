2b48ed4f181a8cdaa07071d0828256a12a681251
Kevin A. Mitchell <kam@datalogics.com>
2021-08-06 22:43:21 -0500

    Initial checkin of bug demonstration

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install a release doxygen
+conan install -if doxygen doxygen/1.9.1@ --build missing
+
+# Export pkg-a, which build_requires doxygen
+conan export pkg-a
+
+# Build pkg-b in debug.
+# This _should_ use the release doxygen, but finds two doxygens instead and then
+# gives an error about a missing folder.
+
+conan install pkg-b -if build -s build_type=Debug --build missing
